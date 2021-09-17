708b4052b573944000e43d2af8af6cf735e162a2
poojaprakas <89781163+poojaprakas@users.noreply.github.com>
2021-09-17 07:44:09 -0700

    Add files via upload

---
+++ AgeOfPerson.java
@@ -0,0 +1,14 @@
+package com.xworkz.controlStaments;
+
+public class AgeOfPerson {
+	public static int getAge() {
+	return 88;	
+	}
+
+	public static void main(String[] args) {
+	int age = AgeOfPerson.getAge();
+	System.out.println("age of person is" +age);
+
+	}
+
+}
